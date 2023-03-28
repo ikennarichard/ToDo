f534b9bf08540f35ff963c2b0707be0218b33d1d
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-27 23:54:02 -1100

    Add web dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
 };
