14de0090e6a1a242a7d8d9d5774aa522756fa3c4
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-28 21:57:12 -1100

    Implement display functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import setList from './modules/Helpers.js';
+import { setList, sortedTasks } from './modules/Helpers.js';
+import { addTask, deleteTask } from './modules/CrudMethods.js';
 import './style.css';
 
 const list = document.querySelector('.list');
+const form = document.querySelector('form');
+const inputField = document.querySelector('.list_input');
 
-function displayList() {
-  list.innerHTML = setList;
+function displayList(arr) {
+  list.innerHTML = setList(arr);
 }
-window.onload = displayList();
+
+function clearInputs() {
+  inputField.value = '';
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addTask(sortedTasks, inputField.value);
+  displayList(sortedTasks);
+  clearInputs();
+});
+
+list.addEventListener('click', (e) => {
+  const target = e.target.closest('.fa-ellipsis-vertical');
+  const trash = e.target.closest('.fa-trash-can');
+  if (target) {
+    target.nextElementSibling.classList.add('show');
+    target.style.display = 'none';
+  } else if (trash) {
+    trash.parentElement.remove();
+  }
+});
+
+window.onload = displayList(sortedTasks);
