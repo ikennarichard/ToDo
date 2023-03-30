b52a5ac6fae859e0215f848c1dc6906700f06b10
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-29 13:44:40 -1100

    Add listeners to manage state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import { setList, sortedTasks } from './modules/Helpers.js';
-import { addTask, deleteTask } from './modules/CrudMethods.js';
+import Helpers from './modules/Helpers.js';
 import './style.css';
+
+const task = new Helpers();
 
 const list = document.querySelector('.list');
 const form = document.querySelector('form');
 const inputField = document.querySelector('.list_input');
-
-function displayList(arr) {
-  list.innerHTML = setList(arr);
-}
 
 function clearInputs() {
   inputField.value = '';
@@ -16,20 +13,33 @@ function clearInputs() {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  addTask(sortedTasks, inputField.value);
-  displayList(sortedTasks);
+  task.addTask(task.taskList.length + 1, document.querySelector('.list_input').value);
   clearInputs();
 });
 
 list.addEventListener('click', (e) => {
   const target = e.target.closest('.fa-ellipsis-vertical');
   const trash = e.target.closest('.fa-trash-can');
+  const description = e.target.closest('.description');
+
   if (target) {
-    target.nextElementSibling.classList.add('show');
+    target.nextElementSibling.classList.toggle('show');
     target.style.display = 'none';
-  } else if (trash) {
-    trash.parentElement.remove();
+  }
+  if (trash) {
+    task.removeTask(Number(trash.id));
+  }
+
+  if (description) {
+    task.editTask(description);
   }
 });
 
-window.onload = displayList(sortedTasks);
+list.addEventListener('focusout', (e) => {
+  const description = e.target.closest('.description');
+  if (description) {
+    task.displayEditedTask(description, Number(description.dataset.id));
+  }
+});
+
+window.onload = task.displayList();
