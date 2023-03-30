d2c253e87a1b797b29eab530a828a80f24a2693b
Ikenna Richard <oguejioforrichard@gmail.com>
2023-03-29 14:37:56 -1100

    Fix script end of line

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,7 +1,7 @@
 export default class Task {
-  constructor(index, description, completed=false) {
+  constructor(index, description, completed = false) {
     this.index = index;
     this.completed = completed;
     this.description = description;
-  };
+  }
 }
