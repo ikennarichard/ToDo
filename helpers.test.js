fe315fa542a610259bb2b37f73c6fcf4138f672a
Harriet Oteng <harriet.oteng@gmail.com>
2023-04-05 13:49:50 +0000

    Add delete test functions

--- helpers.test.js
+++ helpers.test.js
@@ -17,3 +17,19 @@ describe('add list item', () => {
     expect(list).toHaveLength(1);
   });
 });
+
+describe('delete list item', () => {
+  const task = new Helpers();
+  document.body.innerHTML = '<ul class="list"></ul>';
+  task.addTask(task.taskList.length + 1, 'Task 1');
+
+  test('remove task from array', () => {
+    task.removeTask(1);
+    expect(task.taskList.length).toBe(0);
+  });
+
+  test('remove task from dom element', () => {
+    const list = document.querySelectorAll('.list li');
+    expect(list).toHaveLength(0);
+  });
+});
