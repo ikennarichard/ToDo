67d8cdc9ac1e3bcc22a52f79e9b980bb021a5d2c
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-06 05:03:14 -1100

    Add test for status check

--- helpers.test.js
+++ helpers.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import Helpers from './src/modules/Helpers.js';
+import Interactives from './src/modules/Interactives.js';
 
 // describe('add list item', () => {
 //   const task = new Helpers();
@@ -30,10 +31,20 @@ import Helpers from './src/modules/Helpers.js';
 
 describe('status and content updates', () => {
   const task = new Helpers();
+  const utils = new Interactives();
+
   document.body.innerHTML = '<ul class="list"></ul>';
+
   test('editing task items', () => {
     task.addTask(task.taskList.length + 1, 'Task 1');
     task.editTaskList('Edit task', 1);
     expect(task.taskList[0].description).toBe('Edit task');
+  });
+
+  test('check completed status', () => {
+    document.body.innerHTML = "<input class='check' type='checkbox' checked>";
+    const item = document.querySelector('.check');
+    utils.toggleCompleted(1, task.taskList, item);
+    expect(task.taskList[0].completed).toBe(true);
   });
 });
