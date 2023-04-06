a9f156610f289a7a832b5f6596fc0dcc01c6647d
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-06 05:20:43 -1100

    Fix test issues

--- helpers.test.js
+++ helpers.test.js
@@ -4,38 +4,38 @@
 import Helpers from './src/modules/Helpers.js';
 import Interactives from './src/modules/Interactives.js';
 
-// describe('add list item', () => {
-//   const task = new Helpers();
-//   // document.body.innerHTML = '<ul class="list"></ul>';
-//   task.addTask(task.taskList.length + 1, 'Task 1');
+describe('add list item', () => {
+  const task = new Helpers();
 
-//   test('Add task to array', () => {
-//     expect(task.taskList).toHaveLength(1);
-//   });
+  test('Add task to array', () => {
+    document.body.innerHTML = '<ul class="list"></ul>';
+    task.addTask(task.taskList.length + 1, 'Task 1');
+    expect(task.taskList).toHaveLength(1);
+  });
 
-//   test('Add task to DOM element', () => {
-//     const list = document.querySelectorAll('.list li');
-//     expect(list).toHaveLength(1);
-//   });
+  test('Add task to DOM element', () => {
+    const list = document.querySelectorAll('.list li');
+    console.log(list);
+    expect(list).toHaveLength(1);
+  });
 
-//   test('remove task from array', () => {
-//     task.removeTask(1);
-//     expect(task.taskList.length).toBe(0);
-//   });
+  test('remove task from array', () => {
+    task.removeTask(1);
+    expect(task.taskList.length).toBe(0);
+  });
 
-//   test('remove task from dom element', () => {
-//     const list = document.querySelectorAll('.list li');
-//     expect(list).toHaveLength(0);
-//   });
-// });
+  test('remove task from dom element', () => {
+    const list = document.querySelectorAll('.list li');
+    expect(list).toHaveLength(0);
+  });
+});
 
 describe('status and content updates', () => {
   const task = new Helpers();
   const utils = new Interactives();
 
-  document.body.innerHTML = '<ul class="list"></ul>';
-
   test('editing task items', () => {
+    document.body.innerHTML = '<ul class="list"></ul>';
     task.addTask(task.taskList.length + 1, 'Task 1');
     task.editTaskList('Edit task', 1);
     expect(task.taskList[0].description).toBe('Edit task');
