7c55f6adf04a2a5f630db39c2bbfb981ab627815
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-06 09:17:24 -1100

    Refactor code

--- helpers.test.js
+++ helpers.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import Helpers from './src/modules/Helpers.js';
-import Interactives from './src/modules/Interactives.js';
+// import Interactives from './src/modules/Interactives.js';
 
 describe('add list item', () => {
   const task = new Helpers();
@@ -31,24 +31,11 @@ describe('add list item', () => {
 
 describe('status and content updates', () => {
   const task = new Helpers();
-  const utils = new Interactives();
 
   test('editing task items', () => {
     document.body.innerHTML = '<ul class="list"></ul>';
     task.addTask(task.taskList.length + 1, 'Task 1');
     task.editTaskList('Edit task', 1);
     expect(task.taskList[0].description).toBe('Edit task');
-  });
-
-  test('check completed status', () => {
-    document.body.innerHTML = "<input class='check' type='checkbox' checked>";
-    const item = document.querySelector('.check');
-    utils.toggleCompleted(1, task.taskList, item);
-    expect(task.taskList[0].completed).toBe(true);
-  });
-
-  test('clear all completed', () => {
-    task.taskList = utils.clearCompleted(task.taskList);
-    expect(task.taskList).toHaveLength(0);
   });
 });
